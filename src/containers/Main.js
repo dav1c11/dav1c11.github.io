3003b3185716223cdd5d3d7cf28b522453bb0b1a
Aditya <adi.dg.26@gmail.com>
2023-09-03 01:55:40 -0700

    theme fixing

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -23,7 +23,8 @@ import "./Main.scss";
 
 const Main = () => {
   const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
-  const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
+  const [setIsDark] = useLocalStorage(darkPref.matches);
+  const isDark = true;
   const [isShowingSplashAnimation, setIsShowingSplashAnimation] =
     useState(true);
 
